7162cd9daa57168e855b3624f2213d00a2a1cee6
root <root@localhost.localdomain>
2019-01-02 20:37:45 +0800

    auto

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i "s|input_your_key|$LANPROXY_KEY|g" /lanproxy_client/distribution/proxy-client-0.1/conf/config.properties
-sed -i "s|input_your_host|$LANPROXY_HOST|g" /lanproxy_client/distribution/proxy-client-0.1/conf/config.properties
+sed -i "s|input_your_key|$LANPROXY_KEY|g" /lanproxy-client/distribution/proxy-client-0.1/conf/config.properties
+sed -i "s|input_your_host|$LANPROXY_HOST|g" /lanproxy-client/distribution/proxy-client-0.1/conf/config.properties
